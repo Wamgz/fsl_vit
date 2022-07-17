85309b63c8fc546af17ff8a6e69b5517a6fe01af
wangzi <623585585@qq.com>
2022-07-17 20:59:31 +0800

    config: tpn

---
+++ src/run_script/tpn.sh
@@ -0,0 +1,18 @@
+# coding=UTF-8
+python ../train.py \
+--dataset_root ~/WZ/data \
+--epochs 1000 \
+--dataset_name miniImagenet \
+--model_name tpn \
+--classes_per_it_tr 5 \
+--num_support_tr 5 \
+--num_query_tr 15 \
+--classes_per_it_val 5 \
+--num_support_val 5 \
+--num_query_val 15 \
+--height 84 \
+--width 84 \
+--iterations 1000 \
+--learning_rate 0.001 \
+--cuda 1 \
+--comment "tpn"
