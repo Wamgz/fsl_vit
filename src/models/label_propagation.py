d85179a5c4e8293ba4a4272cb8ccff5ae422df22
wangzi <623585585@qq.com>
2022-07-19 13:02:42 +0800

    bugfix

--- src/models/label_propagation.py
+++ src/models/label_propagation.py
@@ -349,7 +349,7 @@ class LabelPropagationVit(nn.Module):
 
         # Step1: Embedding
         inp = torch.cat((support, query), 0)  # (100, 3, 84, 84) 将suport和query set concat在一块
-        emb_all = self.encoder(inp).view(-1, 1600)  # (100, 1600) 合并在一起提取特征
+        emb_all = self.encoder(inp).view(100, -1)  # (100, 1600) 合并在一起提取特征
         N, d = emb_all.shape[0], emb_all.shape[1]
 
         # Step2: Graph Construction
