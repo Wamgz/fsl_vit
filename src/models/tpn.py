f749f7c1939e084e8d18c71ce755ea0c9a243306
wangzi <623585585@qq.com>
2022-07-19 20:18:10 +0800

    feat: log

--- src/models/tpn.py
+++ src/models/tpn.py
@@ -119,8 +119,10 @@ class LabelPropagation(nn.Module):
         """
         # init
         eps = np.finfo(float).eps
+        print('before labels', labels)
         labels = self._map2ZeroStart(labels)
         labels_unique, _ = torch.sort(torch.unique(labels))
+        print('after labels', labels)
 
         ## 拆分support和query，加上对应的class_embedding
         support_idxs, query_idxs = self._support_query_data(labels)
@@ -133,7 +135,7 @@ class LabelPropagation(nn.Module):
 
         # Step1: Embedding
         inp = torch.cat((support, query), 0)  # (100, 3, 84, 84) 将suport和query set concat在一块
-        print('inp', inp)
+        # print('inp', inp)
         emb_all = self.encoder(inp).view(-1, 1600)  # (100, 1600) 合并在一起提取特征
         N, d = emb_all.shape[0], emb_all.shape[1]
 
@@ -165,7 +167,7 @@ class LabelPropagation(nn.Module):
         D1 = torch.unsqueeze(D_sqrt_inv, 1).repeat(1, N)  # (100, 100)
         D2 = torch.unsqueeze(D_sqrt_inv, 0).repeat(N, 1)  # (100, 100)
         S = D1 * W * D2
-        print('S', S)
+        # print('S', S)
 
         # Step3: Label Propagation, F = (I-\alpha S)^{-1}Y
         ys = s_labels  # (25, 5)
